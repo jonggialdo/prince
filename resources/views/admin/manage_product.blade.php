1075901b13a9e1445175b07bde12f3bf17617ead
linda <nmlinda97@gmail.com>
2018-05-09 13:46:14 +0700

    product view blm diedit

--- resources/views/admin/manage_product.blade.php
+++ resources/views/admin/manage_product.blade.php
@@ -6,12 +6,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Manage User</h1>
+            <h1 class="m-0 text-dark">Manage Product</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="{{ route('admin') }}">Home</a></li>
-              <li class="breadcrumb-item active">Manage User</li>
+              <li class="breadcrumb-item active">Manage Product</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
