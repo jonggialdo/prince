af1f4816846f0e65be14f828725dca6067c6feef
linda <nmlinda97@gmail.com>
2018-06-25 18:51:33 +0700

    lnd

--- database/migrations/2018_05_29_034828_create_carts_table.php
+++ database/migrations/2018_05_29_034828_create_carts_table.php
@@ -24,7 +24,6 @@ class CreateCartsTable extends Migration
             $table->integer('id_seller')->default(0);
             $table->string('date_insert')->default('1');
             $table->integer('transaction_status')->default(0);
->>>>>>> 658f06835cc89f5e9da55d6ae859481e53418a72
             $table->timestamps();
         });
     }
