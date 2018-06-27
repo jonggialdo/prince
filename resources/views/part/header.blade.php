05ed2b32318fc9f9b4e3e5fe562269359167238e
rasyidrio <rioalrasyid97@gmail.com>
2018-06-27 17:09:33 +0700

    rio

--- resources/views/part/header.blade.php
+++ resources/views/part/header.blade.php
@@ -8,11 +8,11 @@
       <div class="row">
         <div class="col-lg-12 text-right">
           <div class="logo_container">
-            <a href="/">Prince<span> IPB</span></a>
+            <a href="{{ route('home') }}">Prince<span> IPB</span></a>
           </div>
           <nav class="navbar" style="margin-bottom: 0px; border-bottom-width: 0px; border-right-width: 0px; border-left-width: 0px; border-top-width: 0px; padding-top: 8px; padding-bottom: 0px;">
             <ul class="navbar_menu">
-              <li><a href="/">home</a></li>
+              <li><a href="{{ route('home') }}">home</a></li>
               <li><a href="{{ route('categories') }}">shop</a></li>
               <li><a href="{{ route('contact') }}">contact</a></li>
 
@@ -20,17 +20,6 @@
             <ul class="navbar_user">
             @if (Auth::check())
               <li>
-                <div id="sb-search" class="sb-search">
-                  <form action="{{ url('search')}}">
-                      <input class="form_input input_name input_ph" placeholder="Search..." type="search" value="" name="search" id="search">
-                      
-                      <!-- <input class="sb-search-submit" type="submit" value="">
-                      <span class="sb-icon-search"></span> -->
-
-                  </form>
-                </div>
-              </li>
-              @if (Auth::user()->role_id == 2)
               <div id="sb-search" class="sb-search">
                 <form action="{{ route('searchFP') }}">
                     <input class="form_input input_name input_ph" placeholder="Search..." type="search" value="" name="searchFP" id="searchFP" style="width: 238.979166px;">
@@ -38,6 +27,7 @@
                     <span class="sb-icon-search"></span> -->
                 </form>
               </div></li>
+              
               <li class="checkout">
                 <a href="{{ route('notifikasi_view') }}" style="margin-left: 5px;">
                   <i class="fa fa-bell" aria-hidden="true"></i>
@@ -50,22 +40,8 @@
                     <span id="checkout_items" class="checkout_items"> {{ \App\Cart::where('id_user','=', \Auth::user()->id)->where('checkout_status','=',0)->get()->count() }}</span>
                   </a>
                 </li>
-              @else
-              <li class="checkout">
-                <a href="{{ route('notifikasi_pembeli') }}" style="margin-left: 5px;">
-                  <i class="fa fa-bell" aria-hidden="true"></i>
-                  <span id="checkout_items" class="checkout_items">0</span>
-                </a>
-              </li>
-                <li class="checkout">
-                  <a href="{{ route('cart') }}" style="margin-left: 5px;">
-                    <i class="fa fa-shopping-cart" aria-hidden="true" ></i>
-                    <span id="checkout_items" class="checkout_items"> {{ \App\Cart::where('id_user','=', \Auth::user()->id)->where('checkout_status','=',0)->get()->count() }}</span>
-                  </a>
-                </li>
               @endif
-              @endif
-              <li class = "account" style="padding-left: 0px; width: 100px;">
+              <li class = "account" style="padding-left: 0px; width:100px;">
                 <a href="#" style="margin-left: 5px; margin-right:15px; ">
                   <i class="fa fa-user" aria-hidden="true" style="margin-left: 0px;"></i>
                 </a>
@@ -82,7 +58,7 @@
                     <li><a href="{{ route('profile') }}">Profile</a></li>
                     <li><a href="{{ URL::route('account-sign-out') }}">Logout</a></li>
                   </ul>
-                  @endif
+                @endif
               </li>
             </ul>
             <div class="hamburger_container">
